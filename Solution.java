a6da9483e5e86b443ff9d92457b12958d0a1973e
Kush Saini <kushsaini1212@gmail.com>
2026-03-26 14:10:19 +0530

    infix to prefix

---
+++ Solution.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+class Solution {
+    private int precedence(char c) {
+        if (c == '+' || c == '-') return 1;
+        if (c == '*' || c == '/') return 2;
+        if (c == '^') return 3;
+        return -1;
+    }
+    
+    private String infixToPostfix(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            }
+            else if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            }
+            else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        
+        return result.toString();
+    }
+    
+    public String infixToPrefix(String s) {
+        
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '(') sb.setCharAt(i, ')');
+            else if (sb.charAt(i) == ')') sb.setCharAt(i, '(');
+        }
+        
+        String postfix = infixToPostfix(sb.toString());
+        
+        StringBuilder prefix = new StringBuilder(postfix);
+        return prefix.reverse().toString();
+    }
+}
