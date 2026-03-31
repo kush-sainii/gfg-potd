149c72a25a3f6b247c2f8941dce626354e8fb547
Kush Saini <kushsaini1212@gmail.com>
2026-03-31 21:26:55 +0530

    added kadane algorithm question

---
+++ kadane_algo.java
@@ -0,0 +1,16 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        int max =Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+            if(sum> max){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
