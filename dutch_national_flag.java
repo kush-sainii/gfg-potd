e3e7f4332e69f7f094f57b0c920286e40ec97a5d
Kush Saini <kushsaini1212@gmail.com>
2026-03-31 21:27:57 +0530

    dutch national question

---
+++ dutch_national_flag.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
